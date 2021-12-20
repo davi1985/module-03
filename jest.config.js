421233a4ff3cd38f12d357ccfde6ddc86a212146
Davi Silva <davisilvaphoto@gmail.com>
2021-12-20 18:16:32 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testEnvironment: 'jsdom',
+  displayName: {
+    name: 'root-tests',
+    color: 'cyan',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
