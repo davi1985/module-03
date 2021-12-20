63d21bd7740fc3e35c362b9a251f5c7666a39ef6
Davi Silva <davisilvaphoto@gmail.com>
2021-12-20 15:55:19 -0300

    testing Search component done

--- components/__tests__/search.spec.js
+++ components/__tests__/search.spec.js
@@ -1,5 +1,6 @@
 import Search from '../search';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const doSearch = jest.fn();
 
@@ -18,5 +19,24 @@ describe('Search', () => {
     await fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('shoul render a input type equals search', () => {
+    render(<Search doSearch={doSearch} />);
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
+  });
+
+  it('should call props.doSearch() with the user input', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   });
 });
--- components/search.js
+++ components/search.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 export default function Search({ doSearch }) {
+  const [term, setTerm] = useState('');
+
   return (
     <form
-      onSubmit={() => doSearch()}
+      onSubmit={() => doSearch(term)}
       name="search-form"
       className="relative mt-6 max-w-lg mx-auto"
     >
@@ -18,8 +22,10 @@ export default function Search({ doSearch }) {
       </span>
 
       <input
+        value={term}
+        onChange={(ev) => setTerm(ev.target.value)}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-        type="text"
+        type="search"
         placeholder="Search"
       />
     </form>
